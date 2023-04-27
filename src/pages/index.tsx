e95fa295dfaf7a3d8a619fc28a46bee5c5ceb0d3
tiagorahal <rahal.aires@gmail.com>
2023-04-27 14:02:10 -0300

    Add file upload and display content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,74 +1,30 @@
-import type { NextPage } from "next";
-import React from "react";
+import { useState } from "react";
+export default function Home() {
+  const [text, setText] = useState<string[]>([]);
 
-const Home: NextPage = () => {
-  const [isLoading, setIsLoading] = React.useState(false);
-  const inputFileRef = React.useRef<HTMLInputElement | null>(null);
-
-  const handleOnClick = async (e: React.MouseEvent<HTMLInputElement>) => {
-    /* Prevent form from submitting by default */
-    e.preventDefault();
-
-    /* If file is not selected, then show alert message */
-    if (!inputFileRef.current?.files?.length) {
-      alert("Please, select file you want to upload");
-      return;
+  const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (typeof reader.result === "string") {
+          const lines = reader.result.split("\n");
+          setText(lines);
+        }
+      };
+      reader.readAsText(file);
     }
-
-    setIsLoading(true);
-
-    /* Add files to FormData */
-    const formData = new FormData();
-    Object.values(inputFileRef.current.files).forEach((file) => {
-      formData.append("file", file);
-    });
-
-    /* Send request to our api route */
-    const response = await fetch("/api/upload", {
-      method: "POST",
-      body: formData,
-    });
-
-    const body = (await response.json()) as {
-      status: "ok" | "fail";
-      message: string;
-    };
-
-    alert(body.message);
-
-    if (body.status === "ok") {
-      inputFileRef.current.value = "";
-      // Do some stuff on successfully upload
-    } else {
-      // Do some stuff on error
-    }
-
-    setIsLoading(false);
   };
 
   return (
-    <form className="flex flex-col gap-2">
-      <div className="flex flex-col gap-2">
-        <input
-          type="file"
-          name="myfile"
-          ref={inputFileRef}
-          className="p-2 bg-gray-200 rounded-md"
-          multiple
-        />
-      </div>
-      <div className="flex gap-2 justify-center items-center">
-        <input
-          type="submit"
-          value="Upload"
-          disabled={isLoading}
-          onClick={handleOnClick}
-          className="px-4 py-2 bg-indigo-500 text-white font-semibold rounded-md disabled:opacity-50"
-        />
-        {isLoading && ` Wait, please...`}
-      </div>
-    </form>
+    <div>
+      <input type="file" onChange={handleUpload} />
+      <ul>
+        {text &&
+          text.map((line, index) => {
+            return <li key={index}>{line}</li>;
+          })}
+      </ul>
+    </div>
   );
-};
-
-export default Home;
+}
